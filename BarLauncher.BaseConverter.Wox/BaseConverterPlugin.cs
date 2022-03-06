18d25e66f2f16ef697c17f3f2a90e61c8bdc1360
gissehel <public-devgit-common@gissehel.org>
2022-03-06 10:23:29 +0100

    Add the project ending in .Wox

---
+++ BarLauncher.BaseConverter.Wox/BaseConverterPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarLauncher.BaseConverter.Lib.Service;
+using BarLauncher.EasyHelper;
+using BarLauncher.EasyHelper.Core.Service;
+using BarLauncher.EasyHelper.Service;
+using BarLauncher.EasyHelper.Wox;
+
+namespace BarLauncher.BaseConverter.Wox
+{
+    public class BaseConverterPlugin : WoxPlugin
+    {
+        public override IBarLauncherResultFinder PrepareContext()
+        {
+            var systemInformations = new SystemInformations();
+            var baseConvertService = new BaseConvertService();
+            var systemService = new SystemService("BarLauncher-BaseConverter");
+            var baseResultFinder = new BaseResultFinder(BarLauncherContextService, baseConvertService, systemInformations, systemService);
+
+            return baseResultFinder;
+        }
+    }
+}
